1eed4324a1698a813a18ad227b1fdfd1b366da26
Dana Palazzo <dana@fretless.com>
2019-02-18 11:34:47 -0500

    Add Firebase to the project

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCHv7jrB2I-igfVhbsCi6XaWdXtcRjO9wE',
+  authDomain: 'event-manager-tutorial-55549.firebaseapp.com',
+  databaseURL: 'https://event-manager-tutorial-55549.firebaseio.com',
+  projectId: 'event-manager-tutorial-55549',
+  storageBucket: 'event-manager-tutorial-55549.appspot.com',
+  messagingSenderId: '238304199468'
+}
